d85a9cac86eb9392d5d15d1104614f8187109659
123eliassk <163130474+123eliassk@users.noreply.github.com>
2024-03-26 10:29:46 -0300

    Add files via upload

---
+++ x.js
@@ -0,0 +1,23 @@
+let nombre = "martin" 
+let apellido ="lopezz"
+let edad = 30
+let estatura = 1.66
+let conectado = true
+let desconectado  = false 
+
+let numeritos = [1, 3, 1.60]
+let objeto  = {nombre: "martin", apellido: "lopez", edad: 30, estatura: 1.66}
+
+console.log(objeto)
+
+//declarando los boleanos 
+let registrado = false 
+let humano = true 
+let humanoide = false 
+let logeado = true 
+
+0
+
+
+//declarando arreglo 
+let arreglo = ("martin")
